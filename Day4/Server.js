8ffc2f13bac73395fb7b74acc0fcf6b221799274
AbhiiT7 <90266401+AbhiiT7@users.noreply.github.com>
2022-12-10 12:32:02 +0530

    Create Server.js

---
+++ Day4/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path')
+
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname) +'/index.html');
+});
+ 
+app.listen('3000',()=>{
+
+    console.log(`server is listening on port ${3000}`);
+
+}
+)
